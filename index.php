c9357d3da7780423cb7b555ca5d9ce5037c3096a
Ilya Frolov <1990boot1990@gmail.com>
2020-02-22 18:00:25 +0300

    Задание 0

--- index.php
+++ index.php
@@ -2,13 +2,10 @@
 
 //Домашнее задание #0
 
+include "functions.php";
+
 const TEST_CONSTANTA = 'asd';
 $user_name = "Igor";
-
-function showSomething()
-{
-    return 'SOMETHING';
-}
 
 if (1 == 1) {
     echo 'hi';
